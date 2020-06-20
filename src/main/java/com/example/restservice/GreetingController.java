7ce6b0375d439665b7e138bc5ada3c9aefe4e506
Dong-Hyun Kim <dhkim95@hotmail.com>
2020-06-20 14:29:32 +0900

    ddd

---
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,34 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.rmi.*;
+import com.dhkim9549.mlptictactoe.RMIInterface;
+
+@RestController
+public class GreetingController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(value = "board", defaultValue = "") String board) {
+    
+    System.out.println("board = " + board);
+
+    String a = "";
+    
+    RMIInterface look_up = null;
+    
+    try {
+      look_up = (RMIInterface)Naming.lookup("//bada.ai/MyServer");
+      a += look_up.helloTo(board);
+      Thread.sleep(300);
+    } catch(Exception ex){
+      a += ex;
+    }    
+	
+		return a;
+	}
+}
