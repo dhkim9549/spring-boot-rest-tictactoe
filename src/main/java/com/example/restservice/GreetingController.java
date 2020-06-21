a1ff7eb9e9fe8d87973cc4c9c1f267f765c03880
Dong-Hyun Kim <dhkim95@hotmail.com>
2020-06-21 19:13:15 +0900

    a

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -9,26 +9,39 @@ import org.springframework.web.bind.annotation.RestController;
 import java.rmi.*;
 import com.dhkim9549.mlptictactoe.RMIInterface;
 
+import java.util.*;
+
 @RestController
 public class GreetingController {
 
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(value = "board", defaultValue = "") String board) {
     
-    System.out.println("board = " + board);
+		System.out.println("board = " + board);
 
-    String a = "";
+		String a = "";
     
-    RMIInterface look_up = null;
+		RMIInterface look_up = null;
     
-    try {
-      look_up = (RMIInterface)Naming.lookup("//bada.ai/MyServer");
-      a += look_up.helloTo(board);
-      Thread.sleep(300);
-    } catch(Exception ex){
-      a += ex;
-    }    
-	
+		try {
+			look_up = (RMIInterface)Naming.lookup("//bada.ai/MyServer");
+			a += look_up.helloTo(board);
+			Thread.sleep(300);
+		} catch(Exception ex){
+			a += ex;
+		}    
+
 		return a;
+	}
+
+	@GetMapping("/hello")
+	public Set hello() {
+		TreeSet set = new TreeSet();
+		set.add("kkk");
+		set.add("aaa");
+		set.add("asdfsadf");
+		Random r = new Random();
+		set.add("" + r.nextDouble());
+		return set;
 	}
 }
